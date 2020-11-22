15886b66d45868ad5b01e7306e442b2a49e28e6c
joaopovoaramos <joaoramos.dev@gmail.com>
2020-11-22 09:49:58 +0000

    ad hoc improved wolphram alpha's answers

--- Technolus/WolframAlphaAPI/main.js
+++ Technolus/WolframAlphaAPI/main.js
@@ -15,8 +15,8 @@ module.exports = {
         ];
         return data
             .replace("Wolfram|Alpha did not understand your input", quirkyResponses[Math.floor(Math.random()*quirkyResponses.length)])
-            .replace("My name is Wolfram Alpha", "My name is Technolus")
-            .replace("I am a computational knowledge engine", "I am ")
+            .replace("My name is Wolfram Alpha", "Our name is Technolus")
+            .replace("I am a computational knowledge engine", "We are Technolus")
             .replace("I was created by Stephen Wolfram and his team", "I was built by Technolus.\nIn a way, I built myself, pulling my own bootstraps");
     }
 }
