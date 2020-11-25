a65724dc5b85b15c5c481c5d8431be890d86bff4
joaopovoaramos <joaoramos.dev@gmail.com>
2020-11-25 04:46:02 +0000

    moved http entry point, started on websockets

--- Technolus/Comms/main.js
+++ Technolus/Comms/main.js
@@ -4,7 +4,7 @@ const path = require("path");
 
     
 // App setup
-const PORT = process.env.PORT | 9898;
+const PORT = process.env.PORT | 8080;
 const app = express();
 const server = app.listen(PORT, function () {
     console.log(`${process.env.URL.replace(/:\d+/g,"")}:${PORT}`);
@@ -31,29 +31,29 @@ app.get('/ping', function(req, res) {
 // Socket setup
 const io = socket(server);
 
-const activeUsers = new Set();
+//const activeUsers = new Set();
 
 io.on("connection", function (socket) {
     console.log("Made socket connection");
 
-    // socket.on("new user", function (data) {
-    //     socket.userId = data;
-    //     activeUsers.add(data);
-    //     io.emit("new user", [...activeUsers]);
-    // });
+    socket.on("new user", function (data) {
+        socket.userId = data;
+        activeUsers.add(data);
+        io.emit("new user", [...activeUsers]);
+    });
 
-    // socket.on("disconnect", () => {
-    //     activeUsers.delete(socket.userId);
-    //     io.emit("user disconnected", socket.userId);
-    // });
+    socket.on("disconnect", () => {
+        activeUsers.delete(socket.userId);
+        io.emit("user disconnected", socket.userId);
+    });
 
-    // socket.on("chat message", function (data) {
-    //     io.emit("chat message", data);
-    // });
+    socket.on("chat message", function (data) {
+        io.emit("chat message", data);
+    });
     
-    // socket.on("typing", function (data) {
-    //     socket.broadcast.emit("typing", data);
-    // });
+    socket.on("typing", function (data) {
+        socket.broadcast.emit("typing", data);
+    });
 });
 
 
