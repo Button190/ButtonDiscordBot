d69b62325128af881b5f21d9ee713a92b2d4cae0
joaopovoaramos <joaoramos.dev@gmail.com>
2020-11-25 05:29:10 +0000

    heroku cannot connect to port. fix attempt 1

--- Technolus/Comms/main.js
+++ Technolus/Comms/main.js
@@ -4,59 +4,61 @@ const express = require("express");
 const socket = require("socket.io");
 const path = require("path");
 
-    
-// App setup
-const PORT = process.env.PORT | 8080;
-const app = express();
-const server = app.listen(PORT, function () {
-    console.log(`${process.env.URL.replace(/:\d+/g,"")}:${PORT}`);
-});
-
-
-// Static files
-app.use(express.static(path.join(__dirname,"public")));
-
-// set the home page route
-app.get('/ping', function(req, res) {
-
-    //return an answer just to make it visible on the front end.
-    res.statusCode = 200
-    res.json({
-        status: '200',
-        message: 'pong',
-        host: process.env.URL,
-        local: process.env.LOCAL
-    })
-
-});
-
-// Socket setup
-const io = socket(server);
-
-const activeUsers = new Set();
-
-io.on("connection", function (socket) {
-    console.log("Made socket connection");
-
-    socket.on("new user", function (data) {
-        socket.userId = data;
-        activeUsers.add(data);
-        io.emit("new user", [...activeUsers]);
+(async () => {
+        
+    // App setup
+    const PORT = process.env.PORT | 8080;
+    const app = express();
+    const server = app.listen(PORT, function () {
+        console.log(`${process.env.URL.replace(/:\d+/g,"")}:${PORT}`);
     });
 
-    socket.on("disconnect", () => {
-        activeUsers.delete(socket.userId);
-        io.emit("user disconnected", socket.userId);
+
+    // Static files
+    app.use(express.static(path.join(__dirname,"public")));
+
+    // set the home page route
+    app.get('/ping', function(req, res) {
+
+        //return an answer just to make it visible on the front end.
+        res.statusCode = 200
+        res.json({
+            status: '200',
+            message: 'pong',
+            host: process.env.URL,
+            local: process.env.LOCAL
+        })
+
     });
 
-    socket.on("chat message", function (data) {
-        io.emit("chat message", data);
-    });
-    
-    socket.on("typing", function (data) {
-        socket.broadcast.emit("typing", data);
-    });
-});
+    // Socket setup
+    const io = socket(server);
 
+    const activeUsers = new Set();
+
+    io.on("connection", function (socket) {
+        console.log("Made socket connection");
+
+        socket.on("new user", function (data) {
+            socket.userId = data;
+            activeUsers.add(data);
+            io.emit("new user", [...activeUsers]);
+        });
+
+        socket.on("disconnect", () => {
+            activeUsers.delete(socket.userId);
+            io.emit("user disconnected", socket.userId);
+        });
+
+        socket.on("chat message", function (data) {
+            io.emit("chat message", data);
+        });
+        
+        socket.on("typing", function (data) {
+            socket.broadcast.emit("typing", data);
+        });
+    });
+
+})()
 
 require("./keep-awake.js")
