b154ac8625e9937e351a63ffa5c5597d465efa8f
joaopovoaramos <joaoramos.dev@gmail.com>
2020-11-28 07:14:02 +0000

    created torrents/nyaa

--- Technolus/Torrents/Providers/nyaa.js
+++ Technolus/Torrents/Providers/nyaa.js
@@ -33,57 +33,54 @@ module.exports = {
             return [];
         }
         
-        // ------------------------------------------------------------------------------------------------ I'M HERE !!!!!
         //console.log( "proceeding with results!" );
         
-        let hyperlinks = []
-        $('table>tbody>tr>td>a:nth-child(2)').each( async function(index, element) {
-            hyperlinks.push({
-                title: $(element).text(),
-                href: $(element).attr('href'),
-            });
-        });      
-
-        let magnets = [];
-        flagFound = false;
-        for(let element of hyperlinks ) {
+        
+        $('.default').each(function (index, element) {
             
-            if ( ! flagFound ){
+            // ------------------------------------------------------------------------------------------------ I'M HERE !!!!!
+
+            // 1 - for each default td, get text and magnet
+            // 2 - test the text, if matches 720p, store in array
+
+            let hyperlinks = []
+            $('table>tbody>tr>td>a:nth-child(2)').each( async function(index, element) {
+                hyperlinks.push({
+                    title: $(element).text(),
+                    href: $(element).attr('href'),
+                });
+            });      
+            
+
+            let magnets = [];
+            flagFound = false;
+            for(let element of hyperlinks ) {
                 
-                if ( /720p/.test(element.title) ) {
-                    flagFound = true;
+                if ( ! flagFound ){
                     
-                    const url = baseUrl + element.href;
-                    //console.log( url );
-                    const response = await fetch(url,{
-                        method: 'get',
-                        headers: {
-                            'Content-Type': 'text/html',
-                            'Connection': 'keep-alive',
-                            'user-agent': 'Mozilla/5.0',
-                            
-                        },
-                    });
-                    const html = await response.text();
-                    const $$ = cheerio.load(html);
-                    
-                    $$('.clearfix > ul > li:nth-child(1) > a').each(  function(index, element2) {
+                    if ( /720p/.test(element.title) ) {
+                        flagFound = true;
+                        
+                        const url = baseUrl + element.href;
+                        
                         magnets.push({
                             title: element.title,
                             magnet: $(element2).attr('href'),
                         });
                         //console.log($(element).attr('href'));
                         
-                    });
+                        
+                    }
                     
                 }
-
                 
-
             }
+            
+            
 
 
-        };
+        });
+
         return magnets;
     
     }
